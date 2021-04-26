a99ce9e5988a4f3b06999a26d9dc29f3b4e6e80c
Sadk "YuuGB" Théo saïd <80655913+YuuGB@users.noreply.github.com>
2021-04-26 14:33:33 +0200

    Maj A propos
    Modification de la page A propos sur demande du client

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('message',function(e)
+    {if(e.data.uid&&e.data.type==='sizeUpdate'){
+        var i = document.querySelector('iframe[name="'+e.data.uid+'"]');
+        i.style.width = e.data.payload.width;
+        i.style.height = e.data.payload.height;
+}});
